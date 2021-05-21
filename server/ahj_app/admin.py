96ea8e3661bf6df04a3f25e37152260abd1e38da
Taos Transue <spacechips@rocketmail.com>
2021-05-21 11:08:49 -0700

    Registering all models to admin view

--- server/ahj_app/admin.py
+++ server/ahj_app/admin.py
@@ -1,6 +1,14 @@
 from django.apps import apps
 from django.contrib import admin
 
-
 for model in apps.all_models['ahj_app'].values():
-    admin.site.register(model)
+    model_fields = [field for field in model._meta.get_fields()]
+
+
+    class AdminWithSearch(admin.ModelAdmin):
+        search_fields = [field.name for field in model_fields if field.__class__.__name__ == 'CharField']
+        list_display = search_fields
+        raw_id_fields = [field.name for field in model_fields if field.__class__.__name__ == 'ForeignKey']
+
+    print(AdminWithSearch.raw_id_fields)
+    admin.site.register(model, AdminWithSearch)
